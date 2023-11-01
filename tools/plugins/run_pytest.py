1be2982fb0294373c1036fe2adfc8ac7c78f7ddb
Konstantin Chernyshev <k4black@ya.ru>
2023-11-01 19:38:58 +0100

    wip: plugins design

--- tools/plugins/run_pytest.py
+++ tools/plugins/run_pytest.py
@@ -1,46 +1,7 @@
-from __future__ import annotations
-
-from pydantic import DirectoryPath, Field
-
-from checker.plugins import PluginABC
-from checker.plugins.scripts import RunScriptPlugin
+from checker import PipelinePlugin
 
 
-class RunPytestPlugin(RunScriptPlugin):
-    """Plugin for running pytest."""
-
+class RunPytestPlugin(PipelinePlugin):
     name = "run_pytest"
 
-    class Args(PluginABC.Args):
-        origin: str
-        target: str
-        timeout: int | None = None
-        isolate: bool = False
-        env_whitelist: list[str] = Field(default_factory=lambda: ['PATH'])
-
-        coverage: bool | int | None = None
-
-    def _run(self, args: Args, *, verbose: bool = False) -> str:
-        tests_cmd = ['python', '-m', 'pytest']
-
-        if not verbose:
-            tests_cmd += ['--no-header']
-            tests_cmd += ['--tb=no']
-
-        if args.coverage:
-            tests_cmd += ['--cov-report', 'term-missing']
-            tests_cmd += ['--cov', args.target]
-            if args.coverage is not True:
-                tests_cmd += ['--cov-fail-under', str(args.coverage)]
-        else:
-            tests_cmd += ['-p', 'no:cov']
-
-        run_script_args = RunScriptPlugin.Args(
-            origin=args.origin,
-            script=' '.join(tests_cmd + [args.target]),  # TODO: check, not working when list
-            timeout=args.timeout,
-            isolate=args.isolate,
-            env_whitelist=args.env_whitelist,
-        )
-        result = super()._run(run_script_args, verbose=verbose)
-        return result
+    def __init__(self,
