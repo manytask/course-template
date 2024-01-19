f0b48e897fa8f99eb93746b1034754ce1948a676
Konstantin Chernyshev <k4black@ya.ru>
2024-01-19 18:05:11 +0100

    feat: add templates

--- 2_factorial/factorial.py
+++ 2_factorial/factorial.py
@@ -2,7 +2,9 @@ def factorial(n: int) -> int:
     """
     Calculate the factorial of a number.
     """
+    # SOLUTION BEGIN
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
+    # SOLUTION END
--- 3.ThirdGroup/palindrome/palindrome.py
+++ 3.ThirdGroup/palindrome/palindrome.py
@@ -2,5 +2,7 @@ def is_palindrome(s: str) -> bool:
     """
     Check if a string is a palindrome.
     """
+    # SOLUTION BEGIN
     cleaned_s = "".join(e for e in s if e.isalnum()).lower()
     return cleaned_s == cleaned_s[::-1]
+    # SOLUTION END
